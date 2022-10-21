4ec2d671808cfd10852b38f31d27d681d72e9845
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-21 18:45:58 +0300

    task5

--- if05.py
+++ if05.py
@@ -13,12 +13,15 @@ def main(n):
     x2 = n // 10 % 10
     if x2 > x1:
         max = x2
+
     x3 = n // 100 % 10
     if x3 > x2:
         max = x3
+
     x4 = n // 1000 % 10
     if x4 > x3:
         max = x4
+
     x5 = n // 10000
     if x5 > x4:
         max = x5
